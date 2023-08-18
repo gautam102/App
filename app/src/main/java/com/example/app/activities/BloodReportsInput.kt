aa5a8a2a01f53148bd29d76c33798a600d7a35c1
gautam <gautam.sangeeth15@gmail.com>
2023-08-18 16:26:32 +0530

    urinalysis computation

--- app/src/main/java/com/example/app/activities/BloodReportsInput.kt
+++ app/src/main/java/com/example/app/activities/BloodReportsInput.kt
@@ -6,7 +6,13 @@ import android.os.Bundle
 import com.example.app.databinding.ActivityBloodReportsInputBinding
 import com.example.app.utils.addWatcher
 import com.google.gson.Gson
-
+import com.example.app.utils.thyroidThyroidism
+import com.example.app.utils.thyroidObesity
+import com.example.app.utils.lipidCVD
+import com.example.app.utils.lipidObesity
+import com.example.app.utils.cbcAnemia
+import com.example.app.utils.urinalysisDiabetes
+import com.example.app.utils.cmpDiabetes
 class BloodReportsInput : AppCompatActivity() {
    private lateinit var binding: ActivityBloodReportsInputBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,15 +60,54 @@ class BloodReportsInput : AppCompatActivity() {
         val resultIntent = Intent()
         val sharedPrefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
+        val rbc: Double? = layout1.RBC.text.toString().toDoubleOrNull()
+        val wbc: Double? = layout1.WBC.text.toString().toDoubleOrNull()
+        val hb: Double? = layout1.Hb.text.toString().toDoubleOrNull()
+        val hct: Double? = layout1.Hct.text.toString().toDoubleOrNull()
+        val mcv: Double? = layout1.MCV.text.toString().toDoubleOrNull()
+        val plt: Double? = layout1.Plt.text.toString().toDoubleOrNull()
+
+        val glu: Double? = layout2.Glu.text.toString().toDoubleOrNull()
+        val cal: Double? = layout2.Cal.text.toString().toDoubleOrNull()
+        val sod: Double? = layout2.Sod.text.toString().toDoubleOrNull()
+        val pot: Double? = layout2.Pot.text.toString().toDoubleOrNull()
+        val chl: Double? = layout2.Chl.text.toString().toDoubleOrNull()
+        val alb: Double? = layout2.Alb.text.toString().toDoubleOrNull()
+        val totPrt: Double? = layout2.TotPrt.text.toString().toDoubleOrNull()
+        val alp: Double? = layout2.ALP.text.toString().toDoubleOrNull()
+        val alt: Double? = layout2.ALT.text.toString().toDoubleOrNull()
+        val ast: Double? = layout2.AST.text.toString().toDoubleOrNull()
+        val urea: Double? = layout2.Urea.text.toString().toDoubleOrNull()
+        val creatinine: Double? = layout2.Creatinine.text.toString().toDoubleOrNull()
+
+        val tsh: Double? = layout3.TSH.text.toString().toDoubleOrNull()
+        val t4: Double? = layout3.T4.text.toString().toDoubleOrNull()
+        val t3: Double? = layout3.T3.text.toString().toDoubleOrNull()
+        val ft4: Double? = layout3.FT4.text.toString().toDoubleOrNull()
+        val ft3: Double? = layout3.FT3.text.toString().toDoubleOrNull()
+
+        val totChl: Double? = layout4.TotChl.text.toString().toDoubleOrNull()
+        val hdl: Double? = layout4.HDL.text.toString().toDoubleOrNull()
+        val ldl: Double? = layout4.LDL.text.toString().toDoubleOrNull()
+        val tg: Double? = layout4.TG.text.toString().toDoubleOrNull()
+
         binding.submitButton.setOnClickListener {
             val gson = Gson()
             val editor = sharedPrefs.edit()
 
 
+            val to = thyroidObesity(tsh)
+            val lo = lipidObesity()
+            val ud = urinalysisDiabetes()
+            val cb = cmpDiabetes()
+            val tt = thyroidThyroidism()
+            val ca = cbcAnemia()
+            val lcvd = lipidCVD()
+
             val res = arrayListOf(0)
             var resJson = gson.toJson(res)
             if(resJson == null) { resJson = "" }
-            editor.putString("vitalRes", resJson)
+            editor.putString("bloodReportsRes", resJson)
             editor.apply()
 
             setResult(RESULT_OK, resultIntent)
--- app/src/main/java/com/example/app/activities/UrinalysisInput.kt
+++ app/src/main/java/com/example/app/activities/UrinalysisInput.kt
@@ -1,15 +1,21 @@
 package com.example.app.activities
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import com.example.app.databinding.ActivityUrinalysisInputBinding
 import com.example.app.utils.addWatcher
+import com.google.gson.Gson
+import com.example.app.utils.urinalysisDiabetes
 
 class UrinalysisInput : AppCompatActivity() {
     private lateinit var binding: ActivityUrinalysisInputBinding
-
+    private var glu: Boolean = false
+    private var ket: Boolean = false
+    private var bld: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,17 +24,69 @@ class UrinalysisInput : AppCompatActivity() {
         // add text watchers
         addWatcher(binding.pH, 1.0, 14.0)
         addWatcher(binding.specificGravity, 0.750, 1.2000)
+
+
         // radiogrps
         val bldRG: RadioGroup = binding.bldRG
         val ketRG: RadioGroup = binding.ketRG
         val gluRG: RadioGroup = binding.gluRG
-        // listener for bld radiogrp
-        bldRG.setOnCheckedChangeListener { group, checkId  ->
+
+        gluRG.setOnCheckedChangeListener { group, checkId ->
             val selected: RadioButton = findViewById(checkId)
             when (selected.id) {
-                binding.bldPos.id -> {  }
-                binding.bldNeg.id -> {  }
+                binding.gluPos.id -> {
+                    glu = true
+                }
+
+                binding.gluNeg.id -> {
+                    glu = false
+                }
             }
+        }
+        ketRG.setOnCheckedChangeListener { group, checkId ->
+            val selected: RadioButton = findViewById(checkId)
+            when (selected.id) {
+                binding.ketPos.id -> {
+                    ket = true
+                }
+
+                binding.ketNeg.id -> {
+                    ket = false
+                }
+            }
+        }
+        bldRG.setOnCheckedChangeListener { group, checkId ->
+            val selected: RadioButton = findViewById(checkId)
+            when (selected.id) {
+                binding.bldPos.id -> {
+                    bld = true
+                }
+
+                binding.bldNeg.id -> {
+                    bld = false
+                }
+            }
+        }
+
+        val resultIntent = Intent()
+        val sharedPrefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        binding.button2.setOnClickListener {
+            val gson = Gson()
+            val editor = sharedPrefs.edit()
+
+            val ud = urinalysisDiabetes(glu, ket)
+
+            val res = arrayListOf(0, 0, 0, 0, ud)
+            var resJson = gson.toJson(res)
+            if (resJson == null) {
+                resJson = ""
+            }
+            editor.putString("urinalysisRes", resJson)
+            editor.apply()
+
+            setResult(RESULT_OK, resultIntent)
+            finish()
         }
     }
 }
